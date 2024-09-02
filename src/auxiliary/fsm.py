63000fbf7bc9256537bf1da9ad848592ba66a65c
Sergey <128322450+CraZZy67@users.noreply.github.com>
2024-09-02 14:44:15 +0300

    Не большие доработки доработок

--- src/auxiliary/fsm.py
+++ src/auxiliary/fsm.py
@@ -23,20 +23,22 @@ class ChangeUsername(StatesGroup):
     def change_username(cls, old_value, new_value):
         with open("text/free_layout.txt", "r", encoding="utf-8") as f:
             old_text = "".join(f.readlines())
-        with open("text/free_layout.txt", "w", encoding="utf-8") as f:
-            if old_value in old_text and old_value[0] == "@":
+
+        if old_value in old_text[old_text.find("@"): old_text[old_text.find("@"):].find("\n") + old_text.find("@")]:
+            with open("text/free_layout.txt", "w", encoding="utf-8") as f:
                 new_text = old_text.replace(old_value, new_value)
                 f.write(new_text)
-            else:
-                return False
+        else:
+            return False
 
         with open("text/congratulation.txt", "r", encoding="utf-8") as f:
             old_text = "".join(f.readlines())
-        with open("text/congratulation.txt", "w", encoding="utf-8") as f:
-            if old_value in old_text and old_value[0] == "@":
+
+        if old_value in old_text[old_text.find("@"): old_text[old_text.find("@"):].find(" ") + old_text.find("@")]:
+            with open("text/congratulation.txt", "w", encoding="utf-8") as f:
                 new_text = old_text.replace(old_value, new_value)
                 f.write(new_text)
-            else:
-                return False
+        else:
+            return False
 
         return True
